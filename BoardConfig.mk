51e511ab03377e26fa6227b94a5a2f25c836772e
Lup Gabriel <gwolf2u.website@gmail.com>
2022-10-14 20:33:30 +0300

    beryllium: permissive for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,7 @@ TARGET_SCREEN_DENSITY := 440
 
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/beryllium.config
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
