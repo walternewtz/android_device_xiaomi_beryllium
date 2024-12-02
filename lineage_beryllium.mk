f2c87f9cdbaea7ff39dd9f9e7a11379bc2f1e4b2
walternewtz <walternewtz@outlook.com>
2024-12-02 17:12:41 +0530

    riseup

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -9,12 +9,28 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+RISING_MAINTAINER := Test
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon 845" \
+    RISING_MAINTAINER="Test"
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
+
+RISING_PACKAGE_TYPE := "VANILLA_AOSP"
+
+# Targets
+TARGET_ENABLE_BLUR := false
+TARGET_SUPPORTS_64_BIT_APPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_CHECKELF := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
